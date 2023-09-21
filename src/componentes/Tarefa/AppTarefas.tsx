b84efc3f32e5f4fd9421d5a6c319e4be8ca26215
Luis Gustavo Pereira Nunes <g.luis@academico.ifrn.edu.br>
2023-09-21 10:18:30 -0300

    adicionando  tarefas

--- src/componentes/Tarefa/AppTarefas.tsx
+++ src/componentes/Tarefa/AppTarefas.tsx
@@ -1,25 +1,69 @@
-import List from "@mui/material/List";
+import {
+	Box,
+	FormControl,
+	IconButton,
+	Input,
+	InputAdornment,
+	InputLabel,
+} from "@mui/material";
+import CheckIcon from "@mui/icons-material/Check";
 
-import { InterfaceTarefa } from "../../interfaces/Tarefa";
-import { TarefaListaItem } from "./../Tarefa/TarefaListaItem.tsx";
+import { InterfaceTarefa } from "../interfaces/Tarefa";
+import { TarefasLista } from "./Tarefa/TarefaLista";
+
+const TarefaNova = (props: {
+	tarefa: string;
+	funcaoModificar: (texto: string) => void;
+	funcaoAdicionar: () => void;
+}) => {
+	return (
+		<Box sx={{ maxWidth: "99%" }}>
+			<FormControl fullWidth sx={{ m: 1 }} variant="standard">
+				<InputLabel>Nova tarefa</InputLabel>
+				<Input
+					type={"text"}
+					endAdornment={
+						<InputAdornment position="end">
+							<IconButton onClick={() => props.funcaoAdicionar()}>
+								<CheckIcon />
+							</IconButton>
+						</InputAdornment>
+					}
+					value={props.tarefa}
+					onChange={(e: { target: { value: string; }; }) => props.funcaoModificar(e.target.value)}
+					autoFocus
+					onKeyDown={(e: { key: string; }) => {
+						if (e.key == 'Enter') {
+							props.funcaoAdicionar()
+						}
+					}}
+				/>
+			</FormControl>
+		</Box>
+	);
+};
 
 const AppTarefas = (props: {
-	tarefa: any;
+	tarefa: string;
+	funcaoTarefaNovaModificar: (texto: string) => void;
+	funcaoAdicionar: () => void;
 	tarefas: Array<InterfaceTarefa>;
 	funcaoApagar: (tarefa: InterfaceTarefa) => void;
 	funcaoFinalizar: (id: number) => void;
 }) => {
 	return (
-		<List sx={{ width: "100%", bgcolor: "background.paper" }}>
-			{props.tarefas.map((tarefa) => (
-				<TarefaListaItem
-					key={tarefa.id}
-					tarefa={tarefa}
-					cliqueParaApagar={props.funcaoApagar}
-					cliqueParaFinalizar={props.funcaoFinalizar}
-				/>
-			))}
-		</List>
+		<Box>
+			<TarefaNova 
+				tarefa={props.tarefa}
+				funcaoModificar={props.funcaoTarefaNovaModificar}
+				funcaoAdicionar={props.funcaoAdicionar}
+			/>
+			<TarefasLista
+				tarefas={props.tarefas}
+				funcaoApagar={props.funcaoApagar}
+				funcaoFinalizar={props.funcaoFinalizar}
+			/>
+		</Box>
 	);
 };
 
